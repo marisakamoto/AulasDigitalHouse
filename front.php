ff7bc01663decbda75e13c64e95e74ee1f76cc3d
marisakamoto <43222644+marisakamoto@users.noreply.github.com>
2020-02-08 17:27:13 -0300

    Add files via upload

---
+++ front.php
@@ -0,0 +1,32 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- <meta charset="UTF-8"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+    <title>Formulario</title>
+</head>
+
+<!-- <body> -->
+    <form action="back.php" method="POST">
+        <h1>dsdsds</h1>
+        <div>
+            <label for="nome">Nome: </label>
+            <input type="text" id="nome" name="nomeCliente">
+        </div>
+        <div>
+            <label for="email">Email: </label>
+            <input type="email" , id="email" name="emailCliente">
+        </div>
+        <div>
+            <label for="telefone">Telefone: </label>
+            <input type="number" , id="telefone" name="telefoneCliente">
+        </div>
+        <button>Cadastrar</button>
+    </form>
+</body>
+</html>
